949b5613a107f9a49d34fc7a75d76442c8fd3c71
mistercannor <114514310+mistercannor@users.noreply.github.com>
2025-11-01 17:23:22 -0500

    Add files via upload

---
+++ data/mods/gen9legendsvgc/pokedex.ts
@@ -0,0 +1,127 @@
+export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
+	clefablemega: {
+		inherit: true,
+		abilities: { 0: "Aerilate" },
+	},
+	victreebelmega: {
+		inherit: true,
+		abilities: { 0: "Toxic Debris" },
+	},
+	starmiemega: {
+		inherit: true,
+		baseStats: { hp: 60, atk: 100, def: 105, spa: 130, spd: 105, spe: 120 },
+		abilities: { 0: "Pure Power" },
+	},
+	dragonitemega: {
+		inherit: true,
+		abilities: { 0: "No Guard" },
+	},
+	meganiummega: {
+		inherit: true,
+		abilities: { 0: "Grassy Surge" },
+	},
+	feraligatrmega: {
+		inherit: true,
+		abilities: { 0: "Dragon's Maw" },
+	},
+	ampharosmega: {
+		inherit: true,
+		abilities: { 0: "Fluffy" },
+	},
+	skarmorymega: {
+		inherit: true,
+		abilities: { 0: "Tough Claws" },
+	},
+	mawilemega: {
+		inherit: true,
+		baseStats: { hp: 50, atk: 105, def: 125, spa: 55, spd: 95, spe: 50 },
+	},
+	medichammega: {
+		inherit: true,
+		baseStats: { hp: 60, atk: 100, def: 85, spa: 80, spd: 85, spe: 100 },
+	},
+	gallademega: {
+		inherit: true,
+		abilities: { 0: "Sharpness" },
+	},
+	froslassmega: {
+		inherit: true,
+		abilities: { 0: "Snow Warning" },
+	},
+	emboarmega: {
+		inherit: true,
+		abilities: { 0: "Rock Head" },
+	},
+	excadrillmega: {
+		inherit: true,
+		abilities: { 0: "Sand Rush" },
+	},
+	audinomega: {
+		inherit: true,
+		abilities: { 0: "Regenerator" },
+	},
+	scolipedemega: {
+		inherit: true,
+		abilities: { 0: "Tinted Lens" },
+	},
+	scraftymega: {
+		inherit: true,
+		abilities: { 0: "Intimidate" },
+	},
+	eelektrossmega: {
+		inherit: true,
+		abilities: { 0: "Ion Battery" },
+	},
+	chandeluremega: {
+		inherit: true,
+		abilities: { 0: "Shadow Tag" },
+	},
+	chesnaughtmega: {
+		inherit: true,
+		abilities: { 0: "Protective Thorns" },
+	},
+	delphoxmega: {
+		inherit: true,
+		abilities: { 0: "Levitate" },
+	},
+	greninjamega: {
+		inherit: true,
+		abilities: { 0: "Protean" },
+	},
+	pyroarmega: {
+		inherit: true,
+		abilities: { 0: "Drought" },
+	},
+	dragalgemega: {
+		inherit: true,
+		abilities: { 0: "Adaptability" },
+	},
+	floettemega: {
+		inherit: true,
+		abilities: { 0: "Ange" },
+	},
+	malamarmega: {
+		inherit: true,
+		abilities: { 0: "Contrarian" },
+	},
+	barbaraclemega: {
+		inherit: true,
+		abilities: { 0: "Tough Claws" },
+	},
+	hawluchamega: {
+		inherit: true,
+		abilities: { 0: "Scrappy" },
+	},
+	zygardemega: {
+		inherit: true,
+		abilities: { 0: "Aura Break" },
+	},
+	drampamega: {
+		inherit: true,
+		abilities: { 0: "Drizzle" },
+	},
+	falinksmega: {
+		inherit: true,
+		abilities: { 0: "Brass Bond" },
+	},
+};
